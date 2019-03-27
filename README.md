865744a330a7af57ceef51d92ec097b9cfb9d606
ManuCas <manucastle@hotmail.com>
2019-03-27 12:43:22 -0600

    Add to favorite persisting on new category feature is functional, one click download button function added. Readme updated with info

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # giftastic
-week 6 homework. AJAX and giphy API
+`week 6 homework. AJAX and giphy API`
+
+### Overview
+
+`In this assignment, I use the GIPHY API to make a dynamic web page that populates with gifs of your choice. To achieve this task, the page calls the GIPHY API and use JavaScript and jQuery to change the HTML of the site.
+[Live Demo](https://youtu.be/BqreERTLjgQ)`
+
+##Features
+
+`1. An initial array of  category buttons is created at page load.
+   
+2. When the user clicks on a button, the page  grabs 10 static, non-animated gif images from the GIPHY API and places them on the page.
+
+3. When the user clicks one of the still GIPHY images, the gif animates. If the user clicks the gif again, it stops playing.
+
+4. With every gif, is display its rating (PG, G, etc), gif's title and score. There is one button to add the gif to a Favorite Section an other button to download th gif file
+   * This data is provided by the GIPHY API.
+   
+5. It has a form that takes the value from a user input box and adds it into the category buttons. 
+
+6. If the same category button is pressed again, it allows users to request additional gifs to be added to the page.
+   * Each request adds 10 gifs to the page, and does not  overwrite the existing gifs.(for time responses it is limited to 50 gifs max. It could be limitless)
+
+7. Allows users to add their favorite gifs to a `favorites` section.
+   * This should persist even when they select or add a new topic.`
+   
 
 
+##COMMIT History
 
-
-##'COMMIT History'
-
-commit 1:
+`commit 1:
 repo creation
 
 comit 2:
@@ -26,3 +49,6 @@ added more gif's info
 
 commmit 7:
 request 10 more gifs added whitout clearing
+
+commit 8:
+Add to favorite persisting on new category feature is functional, one click download button function added. Readme updated with info`
--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -29,11 +29,30 @@ $(document).ready(function () {
     });
     $(document).on("click", ".category", displayGif);
     $(document).on("click", ".gifs", animateGif);
-    $(document).on("click", "#add-more", displayGif);
+    // $(document).on("click", "#downloadBtn", function(event){
+    //     event.preventDefault();
+    //     //window.location.href = $(this).attr("data-download");
+    //     window.open($(this).attr("data-download"), '_blank');
+    //    // download($(this).attr("data-download"));
+    //     console.log($(this).attr("data-download"));
+       //});
+ $(document).on("click", "#favoriteBtn", function(event){
+        event.preventDefault();
+        addFavorite(this);
+    });
+
+    $(document).on("click", "#downloadBtn", function(event){
+        
+        event.preventDefault();
+        var name=($(this).attr("data-name"));
+        var x=new XMLHttpRequest();
+	x.open("GET", $(this).attr("data-download"), true);
+	x.responseType = 'blob';
+	x.onload=function(e){download(x.response, name, "image/gif" ); }
+	x.send();
+    });
     
 });//end of document ready
-
-
 
 
 function start() {
@@ -78,7 +97,7 @@ function renderGifs(response) {
         var gifGrid = $("<img >");
         var title = $("<p>");
         var score = $("<p>");
-        var download = $("<button label='download'>");
+        var download = $("<button download>");
         var favorite = $("<button>");
         rating.text("Rating: " + response.data[j].rating);
         gifGrid.attr("src", response.data[j].images.fixed_height_still.url);
@@ -89,7 +108,21 @@ function renderGifs(response) {
         title.text("Title: " + response.data[j].title);
         score.text("Score: " + response.data[j]._score);
         favorite.text("Add to Favorite");
+        favorite.attr("src", response.data[j].images.fixed_height_still.url);
+        favorite.attr("data-still", response.data[j].images.original_still.url);
+        favorite.attr("data-animated", response.data[j].images.fixed_height.url);
+        favorite.attr("data-status", 'still');
+        favorite.attr("data-title", response.data[j].title);
+        favorite.attr("data-score", response.data[j]._score);
+        favorite.attr("data-rating", response.data[j].rating);
+        favorite.attr("id", "favoriteBtn")
         download.text("Download");
+        download.attr("target", "_blank");
+        download.attr("data-download", response.data[j].images.fixed_height.url);
+        download.attr("href", response.data[j].images.fixed_height.url);
+        download.attr("data-name", response.data[j].title)
+        download.attr("id","downloadBtn");
+        
         $(divcont1).append(rating);
         $(divcont1).append(gifGrid);
         $(divcont1).append(title);
@@ -103,7 +136,40 @@ function renderGifs(response) {
     count+=10;
 }
 
+function addFavorite(x){
+    $("#favorites-tag").empty()
+        var divcont2=$("<div class='' id='divcont2' style='height: 370px'>");
+    var tag = $("<h3>");
+    var rating = $("<p>");
+    var gifGrid = $("<img >");
+    var title = $("<p>");
+    var score = $("<p>");
+    var download = $("<button>");
+    
+    rating.text("Rating: " + $(x).attr("data-rating"));
+    title.text("Title: " + $(x).attr("data-title"));
+        score.text("Score: " + $(x).attr("data-score"));
+        gifGrid.attr("src", $(x).attr("src"));
+        gifGrid.attr("data-still", $(x).attr("data-still"));
+        gifGrid.attr("data-animated", $(x).attr("data-animated"));
+        gifGrid.attr("data-status", 'still');
+        gifGrid.addClass("gifs");
+        download.text("Download");
+        download.attr("data-download", $(x).attr("src"));
+        download.attr("href", "no-script.html");
+        download.attr("id","downloadBtn");
+        download.attr("target", "_blank");
+        tag.text("Favorites Section")
+        $(divcont2).append(rating);
+        $(divcont2).append(gifGrid);
+        $(divcont2).append(title);
+        $(divcont2).append(score);
+        $(divcont2).append(download);
 
+        $("#favorites-tag").append(tag);
+        $("#favorites-show").prepend(divcont2);
+
+}
 
 function animateGif() {
     console.log($(this).attr("data-status"));
@@ -120,5 +186,4 @@ function animateGif() {
         $(this).attr("data-status", "still");
     }
 }
-
 
